c4757de1d3a6166609ff54f09976be1531ce47b1
Jakub G <11jakubg@gmail.com>
2023-06-12 13:33:19 +0200

    Revert "error"
    This reverts commit ca745f7e69c81cccc0ea7f56da2eb10d66db0e9b.

--- android/app/src/main/kotlin/com/example/scr2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.scr2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app/home.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(Ekran1());
-}
-
-//theme: _isDarkModeEnabled ? ThemeData.dark() : ThemeData.light(),
