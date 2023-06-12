ca745f7e69c81cccc0ea7f56da2eb10d66db0e9b
Jakub G <11jakubg@gmail.com>
2023-06-12 13:20:07 +0200

    error

---
+++ android/app/src/main/kotlin/com/example/scr2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scr2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:app/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Ekran1());
+}
+
+//theme: _isDarkModeEnabled ? ThemeData.dark() : ThemeData.light(),
